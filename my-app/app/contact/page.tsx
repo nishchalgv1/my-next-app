f6c75dcb0fb84966274ec727d6fa74c4e277cbeb
nishchalgv1 <nischal.15gupta@gmail.com>
2026-05-27 16:02:19 +0530

    nextjs-app

---
+++ my-app/app/contact/page.tsx
@@ -0,0 +1,9 @@
+function Contact(){
+    return (
+        <>
+        This is Contact Page.
+        </>
+    )
+}
+
+export default Contact;
---
+++ my-app/app/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+function RootLayout({children} : {
+  children: React.ReactNode
+}){
+  return (
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
